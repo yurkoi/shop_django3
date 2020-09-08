371c49b1af95b881fb8e445a5394ae5b8144b079
Yurii <yurrchiik@gmail.com>
2020-09-08 20:30:49 +0200

    first init, admin panel, models and main structure are ready

---
+++ shop/shop/mainapp/admin.py
@@ -0,0 +1,54 @@
+from django.contrib import admin
+from django.forms import ModelChoiceField, ModelForm, ValidationError
+from django.utils.safestring import mark_safe
+from .models import *
+from PIL import Image
+
+
+class NotebookAdminForm(ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['image'].help_text = mark_safe('<span style="color:green; font-size:14px;">Load images with minimal resolution {}x{}</span>'
+            .format(
+            *Product.MIN_RESOLUTION
+        ))
+
+    def clean_image(self):
+        image = self.cleaned_data['image']
+        img = Image.open(image)
+        min_height, min_width = Product.MIN_RESOLUTION
+        max_height, max_width = Product.MAX_RESOLUTION
+        if image.size > Product.MAX_IMAGE_SIZE:
+            raise ValidationError('Size of the image can`t be more than 3MB')
+        if img.height < min_height or img.width < min_width:
+            raise ValidationError('Resolution of image less than minimal!')
+        if img.height > max_height or img.width > max_width:
+            raise ValidationError('Resolution of image more than maximum!')
+        return image
+
+
+class NotebookAdmin(admin.ModelAdmin):
+
+    form = NotebookAdminForm
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='notebooks'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+class SmartphoneAdmin(admin.ModelAdmin):
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return ModelChoiceField(Category.objects.filter(slug='smartphones'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Category)
+admin.site.register(Notebook, NotebookAdmin)
+admin.site.register(Smartphone, SmartphoneAdmin)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
---
+++ shop/shop/mainapp/models.py
@@ -0,0 +1,156 @@
+from PIL import Image
+
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+
+#***************
+#1 Category
+#2 Product
+#3 CartProduct
+#4 Cart
+#5 Order
+#***************
+#6 Order
+#7 Specification
+
+User = get_user_model()
+
+
+class MinResolutionErrorExeption(Exception):
+    pass
+
+
+class MaxResolutionErrorExeption(Exception):
+    pass
+
+
+class LatestProductsManager:
+
+    @staticmethod
+    def get_products_for_main_page(*args, **kwargs):
+        with_respect_to = kwargs.get('with_respect_to')
+        products = []
+        ct_models = ContentType.objects.filter(model__in=args)
+        for ct_model in ct_models:
+            model_products = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
+            products.extend(model_products)
+        if with_respect_to:
+            ct_model = ContentType.objects.filter(model=with_respect_to)
+            if ct_model.exists():
+                if with_respect_to  in args:
+                    return sorted(products, key=lambda x:
+                    x.__class__._meta.model_name.startswith(with_respect_to), reverse=True)
+        return products
+
+
+class LatestProducts:
+
+    objects = LatestProductsManager()
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=255, verbose_name='name of category')
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+
+    MAX_IMAGE_SIZE = 3145728
+    MIN_RESOLUTION = (400, 400)
+    MAX_RESOLUTION = (2000, 2000)
+
+    class Meta:
+        abstract = True
+
+    category = models.ForeignKey(Category, verbose_name='category', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, verbose_name='product name')
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(verbose_name='image')
+    descriptions = models.TextField(verbose_name='description', null=True)
+    price = models.DecimalField(max_digits=9, decimal_places=3, verbose_name='price')
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        image = self.image
+        img = Image.open(image)
+        min_height, min_width = Product.MIN_RESOLUTION
+        max_height, max_width = Product.MAX_RESOLUTION
+        if img.height < min_height or img.width < min_width:
+            raise MinResolutionErrorExeption('Resolution of image less than minimum!')
+        if img.height > max_height or img.width > max_width:
+            raise MaxResolutionErrorExeption('Resolution of image more than maximum!')
+        return image
+
+
+class Notebook(Product):
+
+    diagonal = models.CharField(max_length=255, verbose_name='Diagonal')
+    display = models.CharField(max_length=255, verbose_name='display type')
+    processor_freq = models.CharField(max_length=255, verbose_name='processor frequency')
+    ram = models.CharField(max_length=255, verbose_name='RAM')
+    video = models.CharField(max_length=255, verbose_name='videocard')
+    time_without_charge = models.CharField(max_length=255, verbose_name='working time for battery')
+
+    def __str__(self):
+        return '{} : {}'.format(self.category.name, self.title)
+
+
+class Smartphone(Product):
+    diagonal = models.CharField(max_length=255, verbose_name='Diagonal')
+    display = models.CharField(max_length=255, verbose_name='display type')
+    resolution = models.CharField(max_length=255, verbose_name='resolution')
+    accum_volume = models.CharField(max_length=255, verbose_name='battery volume')
+    ram = models.CharField(max_length=255, verbose_name='RAM')
+    sd = models.BooleanField(default=True)
+    sd_volume = models.CharField(max_length=255, verbose_name='Max capacity of sd')
+    main_cam_mp = models.CharField(max_length=255, verbose_name='main camera')
+    frontal_cam_mp = models.CharField(max_length=255, verbose_name='frontal camera')
+
+    def __str__(self):
+        return '{} : {}'.format(self.category.name, self.title)
+
+
+class CartProduct(models.Model):
+
+    user = models.ForeignKey('Customer', verbose_name='customer', on_delete=models.CASCADE)
+    cart = models.ForeignKey("cart", verbose_name='cart', on_delete=models.CASCADE,
+                             related_name='related_products')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", 'object_id')
+    qty = models.PositiveIntegerField(default=1)
+    final_price = models.DecimalField(max_digits=9, decimal_places=3, verbose_name='final price')
+
+    def __str__(self):
+        return f'Cart product {self.product.title}'
+
+
+class Cart(models.Model):
+
+    owner = models.ForeignKey('Customer', verbose_name='owner', on_delete=models.CASCADE)
+    products = models.ManyToManyField(CartProduct, blank=True, related_name='related_cart')
+    total_products = models.PositiveIntegerField(default=0)
+    final_price = models.DecimalField(max_digits=9, decimal_places=3, verbose_name='final price')
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Customer(models.Model):
+
+    user = models.ForeignKey(User, verbose_name='user', on_delete=models.CASCADE)
+    phone =  models.CharField(max_length=20, verbose_name='phone number')
+    address = models.CharField(max_length=255, verbose_name='address')
+
+    def __str__(self):
+        return f'Customer: {self.user.first_name} {self.user.last_name}'
